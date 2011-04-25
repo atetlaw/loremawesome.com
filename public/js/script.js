cdfa106dc6f3e503c26d97e214b62831adc43d9e
Andrew Tetlaw <andrew@tetlaw.id.au>
2011-04-25 19:31:05 +1000

    debugging

--- public/js/script.js
+++ public/js/script.js
@@ -6,7 +6,10 @@ $(function(){
    function lorem_rotate2() {
       $('.awesome').removeClass('awesome-rotate1').addClass('awesome-rotate2');
       setTimeout(lorem_rotate1, 1000);
-    }
+   }
+   $('#theme').bind('loadeddata',function(){console.log('loadeddata')});
+   $('#theme').bind('playing',function(){console.log('playing')});
+   
    $('#theme').bind('playing',function(){lorem_rotate1();});
 });
 
